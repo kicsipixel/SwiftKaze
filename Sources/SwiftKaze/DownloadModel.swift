ceac0aa78149524e23edf183eada5a9cf815475e
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-02-14 17:44:20 +0100

    Document library changed to Temp.

--- Sources/SwiftKaze/DownloadModel.swift
+++ Sources/SwiftKaze/DownloadModel.swift
@@ -3,12 +3,9 @@ import Foundation
 public struct DownloadModel {
     let fileName: String
     
-    public init(fileName: String) {
-        self.fileName = fileName
-    }
-    
-    public func destinationURL(searchPath: FileManager.SearchPathDirectory = .documentDirectory, allowsOverwrite: Bool = false) throws -> URL {
-        let fileURL = try FileManager.default.url(for: searchPath, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(self.fileName)
+    func destinationURL(allowsOverwrite: Bool = false) throws -> URL {
+        let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+        let fileURL = temporaryDirectoryURL.appendingPathComponent(self.fileName)
         
         guard allowsOverwrite == false else { return fileURL }
         guard (try? fileURL.checkResourceIsReachable()) == false else {
@@ -22,4 +19,3 @@ public struct DownloadModel {
 enum FileError: Error {
     case fileAlreadyExists
 }
-
