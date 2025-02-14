06cf6fd634c9190ec6719bb35f3f814658c6a24c
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-02-14 17:21:15 +0100

    DownloadModel was needed public init.

--- Sources/SwiftKaze/DownloadModel.swift
+++ Sources/SwiftKaze/DownloadModel.swift
@@ -3,6 +3,10 @@ import Foundation
 public struct DownloadModel {
     let fileName: String
     
+    public init(fileName: String) {
+        self.fileName = fileName
+    }
+    
     public func destinationURL(searchPath: FileManager.SearchPathDirectory = .documentDirectory, allowsOverwrite: Bool = false) throws -> URL {
         let fileURL = try FileManager.default.url(for: searchPath, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(self.fileName)
         
